7a4819443e9c6f05e7fdaf879e1f3295ec876121
Yamcp <yamis_cp@hotmail.com>
2025-06-17 11:56:00 -0500

    feat: Agregar importaciones para GetX
    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:essenza_app/config/theme/app_theme.dart';
+//getx
+import 'package:get/get.dart';
+//routes
+import 'presentation/routes/routes.dart';
+//firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
