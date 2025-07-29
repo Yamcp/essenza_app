d0332f939e112f9496ae02bb9cf180f944e76549
Yamcp <yamis_cp@hotmail.com>
2025-07-29 09:25:48 -0500

    feat: Refactor controlador y vista de perfil para mejorar la gestión de datos del usuario

--- lib/bloc/controllers/profile_controller.dart
+++ lib/bloc/controllers/profile_controller.dart
@@ -4,19 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfileController extends GetxController {
-  //variables
+  //varibles
   final RxString name = "".obs;
   final RxString email = "".obs;
   final RxString phone = "".obs;
-
+  
   //metodos
-  Future<void> getProfileUid() async {
-    try {
-      //1. Pbtener el uid del usuario
+  Future<void> getProfileUid() async{
+    try{
+
+      //1. Obtener el uid del usuario
       final uid = FirebaseAuth.instance.currentUser?.uid;
-      if (uid !=null){
+      if(uid != null){
         //2. Obtener el usuario
-        final userdb = FirebaseFirestore.instance.collection("users");
+        final userdb = FirebaseFirestore.instance.collection("users"); 
         final user = await userdb.doc(uid).get();
         if(user.exists){
           //3. Obtener los datos del usuario
@@ -24,13 +25,16 @@ class ProfileController extends GetxController {
           name.value = data?["name"] ?? "";
           email.value = data?["email"] ?? "";
           phone.value = data?["phone"] ?? "";
+          debugPrint("name: ${name.value}");
+          debugPrint("email: ${email.value}");
+          debugPrint("phone: ${phone.value}");
         }
       }else{
         debugPrint("No hay usuario autenticado");
       }
-
-    }catch (e) {
+    }catch(e){
       debugPrint(e.toString());
     }
   }
+  
 }
--- lib/presentation/views/profile/profile_view.dart
+++ lib/presentation/views/profile/profile_view.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+//controllers
+import '../../../bloc/controllers/profile_controller.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
+
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  final ProfileController profilecontroller = Get.put(ProfileController());
+  @override 
+  void initState() {
+    super.initState();
+    profilecontroller.getProfileUid();
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Perfil'),
-      ),
-      body: Center(child: Text('Profile'),),
+      appBar: AppBar(title: Text('Perfil')),
+      body: Center(child: Text('Profile')),
     );
   }
 }
